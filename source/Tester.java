01d3c6ecc567a414faba67a1f413a8682b0ef423
naif19ta <juan.patino.dc@gmail.com>
2025-07-04 08:44:23 -0500

    on decolle

---
+++ source/Tester.java
@@ -0,0 +1,13 @@
+/*
+ * jxa - Java executable arguments parser
+ * Jul 4, 2025
+ * Tester file
+ */
+
+public class Tester
+{
+	public static void main(String[] args)
+	{
+	}
+}
+
---
+++ source/jxa/Jxa.java
@@ -0,0 +1,13 @@
+/*
+ * jxa - Java executable arguments parser
+ * Jul 4, 2025
+ * This file gets all the job done :)
+ */
+
+package jxa;
+
+public final class Jxa
+{
+
+}
+
