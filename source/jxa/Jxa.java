59c5137be82234de3a5298e3f20dec3d7a72c521
naif19ta <juan.patino.dc@gmail.com>
2025-07-04 09:24:44 -0500

    getting id's pos

--- source/jxa/Jxa.java
+++ source/jxa/Jxa.java
@@ -8,9 +8,27 @@ package jxa;
 
 public final class Jxa
 {
+	private JxaFlag[] ids = new JxaFlag[26 + 26 + 10];
+	
 	public static void parse (String[] args, JxaFlag[] flags)
 	{
+		checkShortNames(flags);
 		
+	}
+	
+	private static void checkShortNames (JxaFlag[] flags)
+	{
+		for (int i = 0; i < flags.length; i++)
+		{
+			System.out.println(getPosInMap(flags[i].getShortName()));
+		}
+	}
+	
+	private static int getPosInMap (char shortName)
+	{
+		if (Character.isDigit(shortName)) { return shortName - '0'; }
+		if (Character.isLowerCase(shortName)) { return shortName - 'a' + 10; }
+		else { return shortName - 'A' + 36; }
 	}
 }
 
